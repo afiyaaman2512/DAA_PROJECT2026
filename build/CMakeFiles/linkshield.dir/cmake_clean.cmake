43ffbd29a19edf7f348e94251677832577da3d33
Afiya Amanulla <afiyaaman@Afiyas-MacBook-Air.local>
2026-04-08 20:45:29 +0530

    build: add CMakeLists.txt and initial main.cpp

---
+++ build/CMakeFiles/linkshield.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkshield.dir/src/main.cpp.o"
+  "CMakeFiles/linkshield.dir/src/main.cpp.o.d"
+  "linkshield"
+  "linkshield.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linkshield.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
